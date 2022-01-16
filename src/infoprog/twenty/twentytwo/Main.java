01433eeea29e70ceded274486bd3bd1227f27b58
Nukethem12 <61977300+Nukethem12@users.noreply.github.com>
2022-01-16 09:35:43 +0100

    yes2

--- src/infoprog/twenty/twentytwo/Main.java
+++ src/infoprog/twenty/twentytwo/Main.java
@@ -2,8 +2,15 @@ package infoprog.twenty.twentytwo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+
+class sortbytav implements Comparator<Raktar> {
+    public int compare(Raktar a, Raktar b) {
+        return a.tavlapfoldre - b.tavlapfoldre;
+    }
+}
+
+class Raktar { public int lat, lon, zsak, tavlapfoldre;}
 
 public class Main {
 
@@ -12,26 +19,20 @@ public class Main {
     public static int zsaksuly = 20;
     public static int szanteherbiras = 2500;
 
-    public static class Raktar {
-        public int lat;
-        public int lon;
-        public int zsak;
-    }
-
     public static ArrayList<Raktar> raktarak = new ArrayList<>();
-    public static double[] tav = new double[20];
+    public static ArrayList<Integer> tav = new ArrayList<>();
 
     public static void main(String[] args) {
         beolv();
-//        tavszamolas();
-//        for (double d:
-//             tav){
-//            System.out.println(d);
+        tavszamolas();
+//        Collections.sort(raktarak, new sortbytav());
+//        for (Raktar r :
+//                raktarak) {
+//            System.out.println(r.tavlapfoldre);
 //        }
     }
 
-    public static void beolv()
-    {
+    public static void beolv() {
         try {
             System.out.println("Beolvasás");
             File f = new File(filepath);
@@ -65,19 +66,25 @@ public class Main {
     }
 
     public static void tavszamolas(){
-        for (int i = 1; i < 22; i++) {
-            tav[i] = mennyitav(10,20,30,10);
+        for (Raktar r : raktarak) {
+            r.tavlapfoldre = (mennyitav(70,23,r.lat,r.lon));
         }
     }
 
     public static void feladat1() {
         int szanmaxzsakok = szanteherbiras / zsaksuly;
+        for (Raktar r :
+                raktarak) {
+            while (r.zsak != 0) {
+                int zsakok = szanmaxzsakok;
 
+            }
+        }
     }
 
     // forras: https://www.movable-type.co.uk/scripts/latlong.html
     // okosabb verzio
-    public static double mennyitav(int lat1, int lat2, int lon1, int lon2) {
+    public static int mennyitav(int lat1, int lon1, int lat2, int lon2) {
         final double r = 6371e3;
         double Phi1 = lat1 * Math.PI/180;
         double Phi2 = lat2 * Math.PI/180;
@@ -89,7 +96,7 @@ public class Main {
                         Math.sin(dLambda/2) * Math.sin(dLambda/2);
         double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-        return (r * c)/1000; // kilometerben
+        return (int)(r * c)/1000; // kilometerben
     }
 
     // forras: https://www.movable-type.co.uk/scripts/latlong.html
