995ca1b000d3b0bc0f3efc36d4bb8c86003a1e29
Nukethem12 <61977300+Nukethem12@users.noreply.github.com>
2022-01-19 21:26:27 +0100

    Feladat1a,2 befejezve. raktar2.txt teszteleshez

--- src/infoprog/twenty/twentytwo/Main.java
+++ src/infoprog/twenty/twentytwo/Main.java
@@ -3,34 +3,35 @@ package infoprog.twenty.twentytwo;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 class sortbytav implements Comparator<Raktar> {
     public int compare(Raktar a, Raktar b) {
-        return a.tavlapfoldre - b.tavlapfoldre;
+        return a.getTavlapfoldre() - b.getTavlapfoldre();
     }
 }
 
 
 public class Main {
 
-    public static String filepath = "Infoprog2022-main/data files/raktar.txt";
+    private static final String filepath = "data files/raktar2.txt";
     //kilogrammban
-    public static int zsaksuly = 20;
-    public static int szanteherbiras = 2500;
+    private static final int zsaksuly = 20;
+    private static final int szanteherbiras = 2500;
+    private static boolean voltMarIrva;
 
-    public static ArrayList<Raktar> raktarak = new ArrayList<>();
-    public static ArrayList<Integer> tav = new ArrayList<>();
+    private static final ArrayList<Raktar> raktarak = new ArrayList<>();
 
     public static void main(String[] args) {
         beolv();
-//        tavszamolas();
+        tavszamolas();
         System.out.println(mennyiKellMeg());
-//        Collections.sort(raktarak, new sortbytav());
-//        for (Raktar r :
-//                raktarak) {
-//            System.out.println(r.tavlapfoldre);
-//        }
+        feladat1_2();
     }
 
     public static void beolv() {
@@ -44,16 +45,18 @@ public class Main {
                 Scanner sc = new Scanner(scnextlinestring).useDelimiter(",");
                 Raktar r = new Raktar();
                 if (yes) {
-                    r.lat = Integer.parseInt(sc.next());
-                    r.lon = Integer.parseInt(sc.next());
                     yes = false;
+                    r.setLat(Integer.parseInt(sc.next()));
+                    r.setLon(Integer.parseInt(sc.next()));
                 } else if (!sc2.hasNextLine()){
-                    r.lat = Integer.parseInt(sc.next());
-                    r.lon = Integer.parseInt(sc.next());
+                    r.setLat(Integer.parseInt(sc.next()));
+                    r.setLon(Integer.parseInt(sc.next()));
                 } else {
-                    r.lat = Integer.parseInt(sc.next());
-                    r.lon = Integer.parseInt(sc.next());
-                    r.maxcap = Integer.parseInt(sc.next());
+                    r.setLat(Integer.parseInt(sc.next()));
+                    r.setLon(Integer.parseInt(sc.next()));
+                    int maxcapkeszlet = Integer.parseInt(sc.next());
+                    r.setMaxcap(maxcapkeszlet);
+                    r.setKeszlet(maxcapkeszlet);
                 }
                 raktarak.add(r);
                 sc.close();
@@ -68,52 +71,77 @@ public class Main {
 
     public static void tavszamolas(){
         for (Raktar r : raktarak) {
-            r.tavlapfoldre = (mennyiTav(70,23,r.lat,r.lon));
+            r.setTavlapfoldre(mennyiTav(70,23,r.getLat(),r.getLon()));
         }
     }
 
-    public static void feladat1() {
-        int szanmaxzsakok = szanteherbiras / zsaksuly;
-        int szanzsakokszama = szanmaxzsakok;
-        int mennyikell = mennyiKellMeg();
-        int nemnullaraktarszama = 1;
-        int utakszama = 0;
-        int megtettKMk = 0;
-        while (mennyiKellMeg() != 0) {
-        	utakszama++;
-            for (int i = 0; i < raktarak.size(); i++) {
-				// 1 raktar aminek keszlete nem 0
-            	if (raktarak.get(i).keszlet != 0) {
-            		nemnullaraktarszama = i;
-            		break;
-            	}
-			}
-            while (szanzsakokszama != 0) {
-            	if (szanzsakokszama > raktarak.get(nemnullaraktarszama).keszlet) {
-            		szanzsakokszama -= raktarak.get(nemnullaraktarszama).keszlet;
-            		raktarak.get(nemnullaraktarszama).keszlet = 0;
-            		// menjel ma' tovabb
-            		if  (nemnullaraktarszama == raktarak.size()) {
-            			//FileWriter fw = new FileWriter(filepath);
-                 	}
-            	} else {
-            		raktarak.get(nemnullaraktarszama).keszlet -= szanzsakokszama;
-            		szanzsakokszama = 0;
-            		//menjel ma' haza
-            	}
+    public static void feladat1_2(){
+        try {
+            if (raktarak.size() == 0) {
+                beolv();
             }
+            int szanzsakokszama;
+            int mennyikell = mennyiKellMeg();
+            int nemnullaraktarszama;
+            int utakszama = 0;
+            int megtettKMek = 0;
+
+            outer: while (mennyiKellMeg() != 0) {
+                utakszama++;
+                nemnullaraktarszama = legkozelebbiNemNullaRaktar();
+
+                szanzsakokszama= szanteherbiras / zsaksuly;
+                while (szanzsakokszama != 0) {
+
+                    if (szanzsakokszama > raktarak.get(nemnullaraktarszama).getKeszlet()) {
+                        szanzsakokszama -= raktarak.get(nemnullaraktarszama).getKeszlet();
+                        raktarak.get(nemnullaraktarszama).setKeszlet(0);
+                        nemnullaraktarszama++;
+                        // menjel ma' tovabb
+                    } else {
+                        raktarak.get(nemnullaraktarszama).setKeszlet(raktarak.get(nemnullaraktarszama).getKeszlet() - szanzsakokszama);
+                        szanzsakokszama = 0;
+                        //menjel ma' haza
+                    }
+                    if (nemnullaraktarszama == raktarak.size()) {
+                        if (voltMarIrva){
+                            break outer;
+                        }
+                        String s = "," + szanzsakokszama;
+                        Files.write(Paths.get(filepath), s.getBytes(), StandardOpenOption.APPEND);
+                        raktarak.get(nemnullaraktarszama-1).setKeszlet(szanzsakokszama);
+                        System.out.println("Maradek zsakok szama hozza adva a raktar.txt-hez");
+                        voltMarIrva=true;
+                        break outer;
+                    }
+                }
+                mennyikell=mennyiKellMeg();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("lol");
     }
-    
+
     public static int mennyiKellMeg() {
     	int kellendozsak = 0;
     	for (Raktar raktar : raktarak) {
-			kellendozsak += raktar.maxcap;
+			kellendozsak += raktar.getKeszlet();
 		}
     	return kellendozsak;
     }
 
+    public static int legkozelebbiNemNullaRaktar() {
+        int nemnullaraktarszama = 1;
+        for (int i = 0; i < raktarak.size(); i++) {
+            // 1 raktar aminek keszlete nem 0
+            if (raktarak.get(i).getKeszlet() != 0) {
+                nemnullaraktarszama = i;
+                break;
+            }
+        }
+        return nemnullaraktarszama;
+    }
     // forras: https://www.movable-type.co.uk/scripts/latlong.html
     // okosabb verzio
     public static int mennyiTav(int lat1, int lon1, int lat2, int lon2) {
--- src/infoprog/twenty/twentytwo/Raktar.java
+++ src/infoprog/twenty/twentytwo/Raktar.java
@@ -1,24 +1,61 @@
 package infoprog.twenty.twentytwo;
 
 public class Raktar {
-	public int lat; 
-	
+	private int lat;
+	private int lon;
+	private int maxcap; //
+	private int keszlet; // mennyi fer meg
+	private int tavlapfoldre;
 
-	public int lon;
-	public  int maxcap; // 
-	public int keszlet; // mennyi fer meg 
-	public int tavlapfoldre;
-	
 	public Raktar() {
-		
+
 	}
-	
-	public Raktar(int lat, int lon, int maxcap, int keszlet, int tavlapfoldre) {
+
+	public Raktar(int lat, int lon, int maxcap, int tavlapfoldre) {
 		super();
 		this.lat = lat;
 		this.lon = lon;
 		this.maxcap = maxcap;
-		this.keszlet = keszlet;
+		this.keszlet = maxcap;
 		this.tavlapfoldre = tavlapfoldre;
+	}
+
+	public int getLat() {
+		return lat;
+	}
+	public void setLat(int lat) {
+		this.lat = lat;
+	}
+
+	public int getLon() {
+		return lon;
+	}
+	public void setLon(int lon) {
+		this.lon = lon;
+	}
+
+	public int getMaxcap() {
+		return maxcap;
+	}
+	public void setMaxcap(int maxcap)  {
+		this.maxcap = maxcap ;
+	}
+
+	public int getKeszlet() {
+		return keszlet;
+	}
+	public void setKeszlet(int keszlet) {
+		this.keszlet = keszlet;
+	}
+
+	public int getTavlapfoldre() {
+		return tavlapfoldre;
+	}
+	public void setTavlapfoldre(int tavlapfoldre) {
+		this.tavlapfoldre = tavlapfoldre;
+	}
+
+	public int throw_() {
+		throw new RuntimeException("maxcap mar meg volt adva");
 	}
 }
